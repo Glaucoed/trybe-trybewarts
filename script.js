e4b2bbfb36271363626752bfa42b6115ef6cd69b
Glaucoed <gesverzuthe@gmail.com>
2022-07-18 12:03:16 -0300

    ajustes erro lint

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-const loginButton = document.getElementById("submitButton");
-const password = document.getElementById("password");
-const email = document.getElementById("email");
-const submitButton = document.getElementById("submit-btn");
-const agreement = document.getElementById("agreement");
-const textoArea = document.getElementById("textarea");
-const spanArea = document.getElementById("counter");
+const loginButton = document.getElementById('submitButton');
+const password = document.getElementById('password');
+const email = document.getElementById('email');
+const submitButton = document.getElementById('submit-btn');
+const agreement = document.getElementById('agreement');
+const textoArea = document.getElementById('textarea');
+const spanArea = document.getElementById('counter');
+const valorDeArea = textoArea.value.length;
 
 function login() {
-  if (password.value === "123456" && email.value === "tryber@teste.com") {
-    alert("Olá, Tryber!");
+  if (password.value === '123456' && email.value === 'tryber@teste.com') {
+    alert('Olá, Tryber!');
   } else {
-    alert("Email ou senha inválidos.");
+    alert('Email ou senha inválidos.');
   }
 }
-loginButton.addEventListener("click", login);
+loginButton.addEventListener('click', login);
 
 submitButton.disabled = true;
-agreement.addEventListener("click", () => {
+agreement.addEventListener('click', () => {
   if (agreement.checked) {
     submitButton.disabled = false;
   } else {
@@ -25,14 +26,17 @@ agreement.addEventListener("click", () => {
 });
 
 let count = textoArea.maxLength;
-window.onload = () => (spanArea.innerText = count);
+window.onload = () => {
+  spanArea.innerText = count;
+};
 
 const increment = (event) => {
-  if (event.key === "Backspace") {
-    spanArea.innerText = count += 1;
-  } else if (textoArea.value.length >= 0) {
-    let result = textoArea.maxLength - textoArea.value.length;
+  if (event.key === 'Backspace') {
+    count += 1;
+    spanArea.innerText = count;
+  } else if (valorDeArea >= 0) {
+    const result = textoArea.maxLength - textoArea.value.length;
     spanArea.innerText = result;
   }
 };
-textoArea.addEventListener("input", increment);
+textoArea.addEventListener('input', increment);
